f1b10584164181f4ba595862234565d208aa6b45
chrisdombele <christopher.dombele@gmail.com>
2018-03-30 12:11:02 -0400

    adding mini tables

--- Library_DB.sql
+++ Library_DB.sql
@@ -28,7 +28,7 @@ CONSTRAINT Staff_PK PRIMARY KEY (Staff_ID)
 CREATE TABLE Book_t (
     Book_ID		    VARCHAR(50),
 	Author			VARCHAR(50),
-	ISBN 			VARCHAR(17),
+	ISBN 			INT,
 	Cataloged		ENUM("yes", "no"),
 	Holds			INT,
 CONSTRAINT Book_t_PK PRIMARY KEY (Book_ID)
@@ -52,7 +52,7 @@ CONSTRAINT Media_t_PK PRIMARY KEY (Media_ID)
 
 CREATE TABLE Event_t (
 	Event_ID 			   VARCHAR(50),
-	Exhibits_On_Display	   TEXT,
+	Exhibits_On_Display	   INT,
 	Owner_Of_Exhibit 	   VARCHAR(50),
 	Start_Date             DATE,
 	End_Date               DATE,
@@ -73,9 +73,9 @@ CONSTRAINT Student_t_PK PRIMARY KEY (Student_ID)
 );
 
 CREATE TABLE studyRoom_t (
-	studyRoom_ID       VARCHAR(50),
+	studyRoom_ID       INT,
 	Student_ID         VARCHAR(50),
-	timeRequested      INT,
+	timeRequested      TIME,
 CONSTRAINT studyRoom_t_PK PRIMARY KEY (studyRoom_ID),
 CONSTRAINT studyRoom_t_FK1 FOREIGN KEY (Student_ID) REFERENCES Student_t(Student_ID)
 );
@@ -108,7 +108,7 @@ CONSTRAINT staffTransaction_t_PK PRIMARY KEY (Staff_ID, Transaction_ID)
 
 CREATE TABLE staffStudyRoom_t (
   Staff_ID          VARCHAR(10),
-  studyRoom_ID      VARCHAR(50),
+  studyRoom_ID      INT,
 CONSTRAINT staffStudyRoom_t_PK PRIMARY KEY (Staff_ID, studyRoom_ID)
 );
 
@@ -138,7 +138,7 @@ CONSTRAINT staffBook_t PRIMARY KEY (Staff_ID, Book_ID)
 -- End of mini-tables
 
 
--- Start of inserts
+-- Start of main-table inserts
 INSERT INTO Staff_t VALUES
 ('w00985436', 'sheamusp', 'password', 'sheamusp@wit.edu', 'librarian'),
 ('w00988754', 'hellenk', 'cheeseburgers', 'hellenk@wit.edu', 'help desk'),
@@ -147,12 +147,12 @@ INSERT INTO Staff_t VALUES
 ('w00546436', 'crosbyc', 'mechanicalkeyboards', 'crosbyc@wit.edu', 'work study');
 
 INSERT INTO Book_t VALUES
-('Ready Player One', 'Ernest Cline', 8297827818301, 'Book_t', 0),
-('Saga Vol 1', 'John Cena', 8959150715759, 'Book_t', 3),
-('Eye of the World', 'Robert Jordan', 7718800405691, 'Book_t', 3),
-('Black Panther Comic', 'Stan Lee', 6692457568506, 'Book_t', 1),
-('Rogue One', 'George Lucas', 9757089502679, 'Book_t', 0),
-('Cracking the Coding Interview', 'Gayle G', 7079559901159, 'Book_t', 0);
+('Ready Player One', 'Ernest Cline', 8297827818301, 'no', 0),
+('Saga Vol 1', 'John Cena', 8959150715759, 'no', 3),
+('Eye of the World', 'Robert Jordan', 7718800405691, 'yes', 3),
+('Black Panther Comic', 'Stan Lee', 6692457568506, 'no', 1),
+('Rogue One', 'George Lucas', 9757089502679, 'yes', 0),
+('Cracking the Coding Interview', 'Gayle G', 7079559901159, 'yes', 0);
 
 INSERT INTO Transaction_t VALUES
 ('svbx3619jf', 'Ready Player One', 'W00875435', '5018-05-24', '5018-05-24'),
@@ -171,8 +171,8 @@ INSERT INTO Media_t VALUES
 ('After Laughter', 'CD', 7);
 
 INSERT INTO Event_t VALUES
-('Virtual Fractality', 1, 'Ava Poppas', '1-MARCH-5018', '1-APRIL-5018', 'poppasa@wit.edu', '1234567899'),
-('Career Fair', 1, 'Co-op Office', '50-MARCH-5018', '21-MARCH-5018', 'coopoffice@wit.edu', '5034506578');
+('Virtual Fractality', 1, 'Ava Poppas', '1-MARCH-5018', '1-APRIL-5018', 'poppasa@wit.edu', 1234567899),
+('Career Fair', 1, 'Co-op Office', '50-MARCH-5018', '21-MARCH-5018', 'coopoffice@wit.edu', 5034506578);
 
 
 INSERT INTO Student_t VALUES
@@ -184,10 +184,47 @@ INSERT INTO Student_t VALUES
 ('W00124567', 'petersena', 'darkchocolate', 'petersena@wit.edu', 'Cracking the Coding Interview', '5018-05-21', 'Abigail Petersen');
 
 INSERT INTO studyRoom_t VALUES
-('1', 'W00875435', '3:00'),
-('2', 'W00123456', '4:30'),
-('3', 'W00789654', '7:45'),
-('4', 'W00678876', '8:15'),
-('5', 'W00456321', '1:15'),
-('6', 'W00124567', '2:30');
--- End of inserts
+(1, 'W00875435', '3:00'),
+(2, 'W00123456', '4:30'),
+(3, 'W00789654', '7:45'),
+(4, 'W00678876', '8:15'),
+(5, 'W00456321', '1:15'),
+(6, 'W00124567', '2:30');
+-- End of miant-table inserts
+-- start of mini-tables inserts
+INSERT INTO bookTransaction_t VALUES
+('Ready Player One', 'svbx3619jf'),
+('Black Panther Comic', '9hygaqbf5h');
+
+INSERT INTO transactionMedia_t VALUES
+('Star Wars', 'svbx3619jf'),
+('After Laughter', 'nz3uz3kicb');
+
+INSERT INTO studentTransaction_t VALUES
+('W00875435', '7rtg3kqqeq'),
+('W00456321', '9hygaqbf5h');
+
+INSERT INTO staffTransaction_t VALUES
+('w00985436', 'mm52ti67mb'),
+('w00654709', 'mm52ti67mb');
+
+INSERT INTO staffStudyRoom_t VALUES
+('w00546436', 6),
+('w00654709', 4);
+
+INSERT INTO staffEvent_t VALUES
+('w00546436', 'Career Fair'),
+('w00654709', 'Virtual Fractality');
+
+INSERT INTO staffMedia_t VALUES
+('w00546436', 'The Avengers'),
+('w00654709', 'Scott Pilgrim vs The World');
+
+INSERT INTO staffStudent_t VALUES
+('w00988754', 'W00789654'),
+('w00985476', 'W00456321');
+
+INSERT INTO staffBook_t VALUES
+('w00985476', 'Eye of the World'),
+('w00546436', 'Cracking the Coding Interview');
+-- End of mini-tables inserts
